446e10f2757d7e2458d60ddfb403bc8ffa49ee03
“Elly <e.loel1@protonmail.com>
2021-08-16 16:45:07 +1000

    Uploading all Scrimba projects

---
+++ index.js
@@ -0,0 +1,68 @@
+let deckId
+let computerScore = 0
+let myScore = 0
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
+
+const newDeck = async () => {
+    const response = await fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    const data = await response.json()
+    remainingText.textContent = `Remaining cards: ${data.remaining}`
+    deckId = data.deck_id
+    console.log(deckId)
+}
+
+const drawCard = async () => {
+    const response = await fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    const data = await response.json()
+    remainingText.textContent = `Remaining cards: ${data.remaining}`
+    cardsContainer.children[0].innerHTML = `
+        <img src=${data.cards[0].image} class="card" />
+    `
+    cardsContainer.children[1].innerHTML = `
+        <img src=${data.cards[1].image} class="card" />
+    `
+    const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+    header.textContent = winnerText
+    
+    if (data.remaining === 0) {
+        drawCardBtn.disabled = true
+        if (computerScore > myScore) {
+            // display "The computer won the game!"
+            header.textContent = "The computer won the game!"
+        } else if (myScore > computerScore) {
+            // display "You won the game!"
+            header.textContent = "You won the game!"
+        } else {
+            // display "It's a tie game!"
+            header.textContent = "It's a tie game!"
+        }
+    }
+}
+
+let determineCardWinner = (card1, card2) => {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        computerScore++
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
+        return "Computer wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        myScore++
+        myScoreEl.textContent = `My score: ${myScore}`
+        return "You win!"
+    } else {
+        return "War!"
+    }
+}
+
+newDeckBtn.addEventListener("click", newDeck)
+drawCardBtn.addEventListener("click", drawCard)
